c547b869c64232db5ccffef387b98bf231b142be
Patrick Shobe <patrick@patrickshobe.com>
2018-10-24 14:54:30 -0600

    Adds pull request template and Adds additional interface spec

--- spec/services/trelora_api_interface_spec.rb
+++ spec/services/trelora_api_interface_spec.rb
@@ -6,13 +6,35 @@ describe 'TRELORA API Interface' do
     expect(trelora).to be_a(TreloraApiInterface)
   end
 
-  it 'should complete a successful post' do
-    VCR.use_cassette('trelora_post') do
+  it 'should complete a successful member post' do
+    VCR.use_cassette('trelora_member_post') do
       trelora = TreloraApiInterface.new
-      body = { email: 'steven@trel.co',
+      body = { email:    'steven@trel.co',
                password: 'password' }
-      response = trelora.post(:members, body)
-      expect(response.status).to eq(200)
+      member_response = trelora.post(:members, body)
+      expect(member_response.status).to eq(200)
+    end
+  end
+  it 'should complete a successful member post' do
+    VCR.use_cassette('trelora_properties_post') do
+      trelora = TreloraApiInterface.new
+      body = {
+                'HTTP_AUTH_TOKEN' => 'this_is_a_very_simple_auth_token_string',
+                'address' => '1860_south_marion_street-Denver-CO-80210'
+      }
+      properties_response = trelora.post(:properties, body)
+      expect(properties_response.status).to eq(200)
+    end
+  end
+  it 'should complete a successful update post' do
+    VCR.use_cassette('trelora_update_post') do
+      trelora = TreloraApiInterface.new
+      body = {
+                'HTTP_AUTH_TOKEN' => 'this_is_a_very_simple_auth_token_string',
+                'address' => '1860_south_marion_street-Denver-CO-80210'
+      }
+      properties_response = trelora.post(:properties, body)
+      expect(properties_response.status).to eq(200)
     end
   end
 end
