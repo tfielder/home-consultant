9fee49e266b5cf1ab98ce789b0067275c4a2d12b
Daniel Mulitauopele <daniel.mulitauopele@gmail.com>
2018-10-26 11:08:14 -0600

    changes more double quotes to single quotes

--- spec/features/user_logs_in_spec.rb
+++ spec/features/user_logs_in_spec.rb
@@ -1,8 +1,8 @@
 require 'rails_helper'
 
 describe 'user visits log in page' do
-  it "can see log in page" do
-    visit "/"
+  it 'can see log in page' do
+    visit '/'
 
     expect(page).to have_content('Trelora Listing Consultation Tool')
     expect(page).to have_content('Member Email')
@@ -11,10 +11,10 @@ describe 'user visits log in page' do
   end
 
   it "can log in a user with a correct password" do
-    visit "/"
+    visit '/'
 
-    fill_in "user[address]", with: 'steven@trel.co'
-    fill_in "password", with: 'password'
+    fill_in 'user[address]', with: 'steven@trel.co'
+    fill_in 'password', with: 'password'
 
     VCR.use_cassette('valid login') do
       click_on 'Log in'
@@ -25,36 +25,40 @@ describe 'user visits log in page' do
     expect(page).to have_button('Find Location')
   end
 
-  it "cannot log in a user with invalid password" do
-    visit "/"
+  it 'cannot log in a user with invalid password' do
+    visit '/'
 
-    fill_in "user[address]", with: 'steven@trel.co'
-    fill_in "password", with: ''
+    fill_in 'user[address]', with: 'steven@trel.co'
+    fill_in 'password', with: ''
 
     VCR.use_cassette('user_without_password') do
       click_on 'Log in'
     end
 
     expect(current_path).to eq(root_path)
-    expect(page).to have_content('422 error. Member does not exist! 🔐. Invalid Login. Please try again.')
+    expect(page).to have_content(
+      '422 error. Member does not exist! 🔐. Invalid Login. Please try again.'
+    )
     expect(page).to have_content('Trelora Listing Consultation Tool')
     expect(page).to have_content('Member Email')
     expect(page).to have_content('Password')
     expect(page).to have_button('Log in')
   end
 
-  it "cannot log in a user with invalid email" do
-    visit "/"
+  it 'cannot log in a user with invalid email' do
+    visit '/'
 
-    fill_in "user[address]", with: 'steven'
-    fill_in "password", with: 'password'
+    fill_in 'user[address]', with: 'steven'
+    fill_in 'password', with: 'password'
 
     VCR.use_cassette('user_without_email') do
       click_on 'Log in'
     end
 
     expect(current_path).to eq(root_path)
-    expect(page).to have_content('422 error. Member does not exist! 🔐. Invalid Login. Please try again.')
+    expect(page).to have_content(
+      '422 error. Member does not exist! 🔐. Invalid Login. Please try again.'
+    )
     expect(page).to have_content('Trelora Listing Consultation Tool')
     expect(page).to have_content('Member Email')
     expect(page).to have_content('Password')
