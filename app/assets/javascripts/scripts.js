dbcefd71d542cb604d7707fba8b83cd608b56b75
Daniel Mulitauopele <daniel.mulitauopele@gmail.com>
2018-10-27 17:22:16 -0600

    Finally got jquery to work. Mega struggle. Will work on refactoring

--- app/assets/javascripts/scripts.js
+++ app/assets/javascripts/scripts.js
@@ -1,16 +1,48 @@
 $(document).ready(function() {
   $('.log-in-submit').on('click', function () {
-    var emailInput = $('#user_address').val();
-    if (emailInput === '') {
-      $('.message').text('Please fill in the email field.');
+    var emailInputElement = $('#user_address')
+    var passwordInputElement = $('#password')
+    var submitButtonElement = $('.log-in-submit')
+
+    const invalidEmailMessage = "Please provide valid email"
+    const userAddressId = "#user_address"
+
+    passwordInputElement.on('click', function (event) {
+      if (isEmail(emailInputElement.val())) {
+        $('.message').text();
+        $(emailInputElement).css({"border-color": "initial"});
+      }
+    })
+
+    submitButtonElement.on('click', function (event) {
+      if (isEmail(emailInputElement.val())) {
+        $('.message').text();
+        $(emailInputElement).css({"border-color": "initial"});
+      }
+    })
+
+    function isEmail(email) {
+      var regex = /^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+      return regex.test(email);
+    }
+
+    // function setInvalidCSS(e) {
+    //   $(e).css({border})
+    // }
+    // function setMessage(e, message) {
+    //
+    // }
+
+    if (!isEmail(emailInputElement.val())) {
+      $('.message').text('Please provide valid email.');
       $("#user_address").css({"border-color": "red"});
+      // setMessage(userAddressId, invalidEmailMessage)
+      // setInvalidCSS(userAddressId)
       event.preventDefault();
-      event.stopPropagation();
-    } else {
+    } else if (passwordInputElement.val() === '') {
       $('.message').text('Please fill in the password field.');
       $("#password").css({"border-color": "red"});
       event.preventDefault();
-      event.stopPropagation();
-    }
+    } else {}
   });
 });
