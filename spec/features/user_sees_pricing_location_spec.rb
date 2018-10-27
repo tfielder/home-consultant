7f097f1e27e5d439fe1bf9cdd597478b418c2577
Tim Fielder <fiel1139@gmail.com>
2018-10-27 13:16:04 -0600

    updates development environment to not check yarn integrity.

--- spec/features/user_sees_pricing_location_spec.rb
+++ spec/features/user_sees_pricing_location_spec.rb
@@ -4,6 +4,20 @@ describe 'as a user' do
   describe 'visiting the pricing location screen' do
     it 'shows the pricing location details' do
       visit ''
+
+      expect(page).to have_content("Name:")
+      expect(page).to have_content("Email:")
+      expect(page).to have_content("Phone:")
+      expect(page).to have_content("Map:")
+      expect(page).to have_content("Opted In:")
+      expect(page).to have_content("Found Us:")
+      expect(page).to have_content("Estimate:")
+      expect(page).to have_content("Enthusiasm:")
+      expect(page).to have_content("Timing:")
+      expect(page).to have_content("Adopter Type:")
+      expect(page).to have_content("Updates:")
+      expect(page).to have_content("Stage:")
+      expect(page).to have_content("Retainer:")
     end
   end
 end
