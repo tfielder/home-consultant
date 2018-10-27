54d1200f2a0e039ec15eb006cbbe76c576ac62d3
Tim Fielder <fiel1139@gmail.com>
2018-10-27 12:52:33 -0600

    adds user sees pricing location spec.

---
+++ spec/features/user_sees_pricing_location_spec.rb
@@ -0,0 +1,9 @@
+require 'rails_helper'
+
+describe 'as a user' do
+  describe 'visiting the pricing location screen' do
+    it 'shows the pricing location details' do
+      visit ''
+    end
+  end
+end
