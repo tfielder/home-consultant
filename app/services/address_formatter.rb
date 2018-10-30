2565dfa1d19849c640790a55778917ebb4da79db
Tim Fielder <fiel1139@gmail.com>
2018-10-30 15:09:17 -0600

    refactors address_formatter.rb

--- app/services/address_formatter.rb
+++ app/services/address_formatter.rb
@@ -1,6 +1,7 @@
 class AddressFormatter
   def initialize(address)
     @address = address
+    @parsed = StreetAddress::US.parse(@address)
   end
 
   def street_hash
@@ -41,20 +42,20 @@ class AddressFormatter
     }
   end
 
-  def parsed
-    parse = StreetAddress::US.parse(@address)
+  def formatter
+    first_piece = [@parsed.number, street_prefix, @parsed.street.downcase, street_type].compact.join("_")
+    second_piece = [@parsed.city, @parsed.state.upcase, @parsed.postal_code].compact.join("-")
+
+    first_piece.concat("-").concat(second_piece)
   end
 
-  def formatter
-    parsed
-    street_type = parsed.street_type.downcase
-    street_type = street_hash[street_type] if street_hash.keys.include? street_type
-    street_prefix = parsed.prefix.downcase
-    street_prefix = cardinal_abbv[street_prefix] if cardinal_abbv.keys.include? street_prefix
+  def street_type
+    typo = @parsed.street_type.downcase
+    typo = street_hash[typo] if street_hash.keys.include? typo
+  end
 
-    first_piece = [parsed.number, street_prefix, parsed.street.downcase, street_type].compact.join("_")
-    second_piece = [parsed.city, parsed.state, parsed.postal_code].compact.join("-")
-
-    form = first_piece.concat("-").concat(second_piece)
+  def street_prefix
+    pre = @parsed.prefix.downcase
+    pre = cardinal_abbv[pre] if cardinal_abbv.keys.include? pre
   end
 end
