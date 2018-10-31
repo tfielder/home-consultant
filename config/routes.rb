7cc4f1b7079b61dc47081c189cc20dae024296ba
Tim Fielder <fiel1139@gmail.com>
2018-10-31 11:28:32 -0600

    adds route for collect view.

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   root 'session#new'
   post '/', to: 'session#create'
   post '/main_page', to: 'main_page#create'
-  resources :main_page, only: [:create, :index, :prepare]
+  resources :main_page, only: [:create, :index, :prepare, :collect]
 end
