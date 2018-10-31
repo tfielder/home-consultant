d5a17fc19c7d6abfbcf121a3db3e00af8fa4df71
andrewetobin <andrewetobin@gmail.com>
2018-10-30 18:34:51 -0600

    power out

--- spec/models/property_facade_spec.rb
+++ spec/models/property_facade_spec.rb
@@ -16,7 +16,74 @@ describe PropertyFacade, type: :model do
     end
     it '#name' do
       expect(@property_facade.name).to eq("Tyler Long")
+    end
 
+    it '#email' do
+      expect(@property_facade.email).to eq("tyler+12@trelora.com")
+    end
+
+    it '#phone' do
+      expect(@property_facade.phone).to eq("3033333333")
+    end
+
+    it '#map' do
+      array = expect(@property_facade.map).to eq("Lat: 39.683019, Long: -104.971964")
+    end
+
+    it '#opted_in' do
+      expect(@property_facade.opted_in).to eq("Yes")
+    end
+
+    it '#found_us' do
+      expect(@property_facade.found_us).to eq("Google Search")
+    end
+
+    it '#zestimate' do
+      expect(@property_facade.zestimate).to eq(704000)
+    end
+
+    it '#z_low' do
+      expect(@property_facade.z_low).to eq(696000)
+    end
+
+    it '#z_high' do
+      expect(@property_facade.z_high).to eq(754000)
+    end
+
+    it '#hj_low' do
+      expect(@property_facade.hj_low).to eq(578000)
+    end
+
+    it '#hj_high' do
+      expect(@property_facade.hj_high).to eq(679000)
+    end
+
+    xit '#hj_avg' do
+      expect(@property_facade).to eq(679000)
+    end
+
+    xit '#enthusiasm' do
+      expect(@property_facade).to eq("Tyler Long")
+    end
+
+    xit '#timing' do
+      expect(@property_facade).to eq("Tyler Long")
+    end
+
+    xit '#adopted_type' do
+      expect(@property_facade).to eq("Tyler Long")
+    end
+
+    xit '#updates' do
+      expect(@property_facade).to eq("Tyler Long")
+    end
+
+    xit '#stage' do
+      expect(@property_facade).to eq("Tyler Long")
+    end
+
+    xit '#retainer' do
+      500
     end
   end
 end
