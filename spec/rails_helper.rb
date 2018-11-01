e65392344f7671f392b8f787f79ff612bb9c3854
Patrick Shobe <patrick@patrickshobe.com>
2018-10-31 22:17:01 -0600

    More cleaning

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -45,6 +45,7 @@ RSpec.configure do |config|
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
 end
+
 VCR.configure do |config|
   config.cassette_library_dir = "fixtures/vcr_cassettes"
   config.hook_into :webmock
