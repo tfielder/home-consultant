fcfcc9d3f5ed9ff4a6c559cd941ead22f9b0ada4
Tim Fielder <fiel1139@gmail.com>
2018-10-31 18:53:40 -0600

    revises create function for collect form.

--- app/controllers/main_page_controller.rb
+++ app/controllers/main_page_controller.rb
@@ -10,6 +10,17 @@ class MainPageController < ApplicationController
       api_call = PropertyService.new(address, auth_token)
       @property_facade = PropertyFacade.new(api_call.response)
     end
+    if params[:about]
+      binding.pry
+      AppointmentCollector.new(params[:about],
+                  params[:recommended_price],
+                  params[:opciones],
+                  params[:commission],
+                  params[:about_seller],
+                  params[:cc_number],
+                  params[:cc_expiration_month][:cc_expires_year]
+                  )
+    end
     render :prepare
   end
 end
