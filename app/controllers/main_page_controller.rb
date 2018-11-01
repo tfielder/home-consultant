29c6a095ca6ce433c2d88584f7193587794ea77f
Tim Fielder <fiel1139@gmail.com>
2018-10-31 19:01:21 -0600

    revises the mainpage controller create action to take appointment info.

--- app/controllers/main_page_controller.rb
+++ app/controllers/main_page_controller.rb
@@ -11,15 +11,20 @@ class MainPageController < ApplicationController
       @property_facade = PropertyFacade.new(api_call.response)
     end
     if params[:about]
+      appointment_info = {}
+      appointment_info[:consultation] = {
+        :address => params[:address],
+        :about_this_home => params[:about_this_home],
+        :client_enthusiasm => params[:client_enthusiasm],
+        :commission => params[:commission],
+        :about_the_seller => params[:about_the_seller],
+        :credit_card => params[:credit_card],
+        :exp_date => params[:exp_date],
+        :price => params[:price]
+      }
       binding.pry
-      AppointmentCollector.new(params[:about],
-                  params[:recommended_price],
-                  params[:opciones],
-                  params[:commission],
-                  params[:about_seller],
-                  params[:cc_number],
-                  params[:cc_expiration_month][:cc_expires_year]
-                  )
+      AppointmentCollector.new(appointment_info, auth_token, email)
+      #need email as a helper
     end
     render :prepare
   end
