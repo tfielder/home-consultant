450a652b955e20d788994a902cb216eef5ad17eb
Daniel Mulitauopele <daniel.mulitauopele@gmail.com>
2018-10-31 22:41:55 -0600

    added conditional to formatter so that all nil entries re-render the page

--- app/controllers/main_page_controller.rb
+++ app/controllers/main_page_controller.rb
@@ -5,12 +5,12 @@ class MainPageController < ApplicationController
   end
 
   def create
-    if params[:address]
+    if AddressFormatter.new(params[:address]).formatter.nil? == false
       address = AddressFormatter.new(params[:address]).formatter
       api_call = PropertyService.new(address, auth_token)
       @property_facade = PropertyFacade.new(api_call.response)
     else
-      flash.now[:error] = ""
+      flash.now[:error] = "That address does not exist/is incomplete. Please try again."
     end
     render :prepare
   end
